006797a32b62c8b0b5a1d326780f3de7287bddb4
RasLin1 <rasmus.a.lindstrom@gmail.com>
2025-10-21 15:38:07 +0300

    Fixed mod_9_teht_4 so that only one car can win

--- mod_09/tehtava_04.py
+++ mod_09/tehtava_04.py
@@ -5,6 +5,8 @@ counter = 0
 auto_amount = 10
 allow_race  =  True
 autot = []
+leading_auto_rekisteri = 0
+leading_auto_distance = 0
 
 while allow_race:
     if counter < auto_amount:
@@ -15,15 +17,18 @@ while allow_race:
         counter = counter + 1
 
     elif counter == auto_amount:
-        for x in autot:
-            if x.kuljettu_matka < 10000:
-                kiihtymis_maara = random.randint (-10, 15)
-                x.kiihdyta(kiihtymis_maara)
-                x.kulje(1)
-                print(f"{x.rekisteritunnus} on kulkenut {x.kuljettu_matka} km")
-            elif x.kuljettu_matka >= 10000:
-                print(f"{x.rekisteritunnus} voitti!!")
+        for auto in autot:
+            kiihtymis_maara = random.randint (-10, 15)
+            auto.kiihdyta(kiihtymis_maara)
+            auto.kulje(1)
+            if auto.kuljettu_matka > leading_auto_distance:
+                leading_auto_rekisteri = auto.rekisteritunnus
+                leading_auto_distance = auto.kuljettu_matka
+            print(f"{auto.rekisteritunnus} on kulkenut {auto.kuljettu_matka} km")
+            if auto.kuljettu_matka >= 10000:
                 allow_race = False
+
+print(f"{leading_auto_rekisteri} voitti ja se kulki {leading_auto_distance} km")
         
 
     
