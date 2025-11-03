03d9fe2d8ec3420eeaf4b2b2aad900758f179b7e
Rasmus <rasmus.a.lindstrom@gmail.com>
2025-11-03 18:18:51 +0200

    Mod 12 done

--- mod_12/tehtavat/tehtava_02.py
+++ mod_12/tehtavat/tehtava_02.py
@@ -4,7 +4,7 @@ from geopy.geocoders import Nominatim
 key = "e660192f564b0a27b33a9b8237f1488f"
 geolocator = Nominatim(user_agent="weather_app")
 geocode = geolocator.geocode(input("Kerro paikkakunnan nimi: "))
-response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?lat={geocode[0]}&lon={geocode[1]}&appid={key}")
+response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={geocode.latitude}&lon={geocode.longitude}&appid={key}")
 data = response.json()
-temp = data["temp"]
-print(f"Lämpö kohteesa on: {temp} kelvin")
+temp_celsius = (data["main"]["temp"] -273.15)
+print(f"Lämpö kohteesa on: {temp_celsius:.2f} °C")
