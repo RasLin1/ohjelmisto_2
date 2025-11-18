5143ba3768757ae600ac4934a3d6a2461c7988fa
RasLin1 <rasmus.a.lindstrom@gmail.com>
2025-11-18 15:27:01 +0200

    Js mod 4 t3 wip

--- javascript/mod_04/tehtava_03/t3.js
+++ javascript/mod_04/tehtava_03/t3.js
@@ -4,7 +4,7 @@
 const mainForm = document.getElementById('mainForm');
 const showsContainer = document.getElementById('showsContainer');
 let i = 1;
-mainForm.addEventListener('submit', async function(evt) {
+async function showRetAndPrint(evt) {
     // ... prevent the default action.
     evt.preventDefault();
     // get value of input element
@@ -13,18 +13,26 @@ mainForm.addEventListener('submit', async function(evt) {
         const response = await fetch(`https://api.tvmaze.com/search/shows?q=${code}`);    // starting data download, fetch returns a promise which contains an object of type 'response'
         const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
         for (let x of jsonData) {
-            
             console.log(x);    // log the result to the console
             const div = document.createElement("div");
-            div.id = `tDiv${i}`;
+                div.id = `tDiv${i}`;
+                div.classList.add('card', 'col-4');
             showsContainer.insertAdjacentElement('beforeend', div);
             const divTarget = document.getElementById(`tDiv${i}`);
             const h2 = document.createElement("h2");
-            h2.textContent = x.show.name;
+                h2.textContent = x.show.name;
+            const a = document.createElement("a");
+                a.textContent = x.show.url;
+                a.target = "_blank";
+            const img = document.createElement("img");
+                img.src = x.show.image.medium;
             divTarget.insertAdjacentElement('beforeend', h2);
+            divTarget.insertAdjacentElement('beforeend', a);
+            divTarget.insertAdjacentElement('beforeend', img);
             i++;
         }
     } catch (error) {
         console.log(error.message);
     }
-});
+}
+mainForm.addEventListener('submit', showRetAndPrint);
